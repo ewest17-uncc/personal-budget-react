207825e114b6b8dde07d50a0553ddcb46b95643f
Evan West <ewest17@uncc.edu>
2020-10-16 12:33:07 +0100

    Brough PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Fabio Nolasco and Evan West
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
